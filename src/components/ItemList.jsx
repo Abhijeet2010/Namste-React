f51877de4209bbd10612352c29a6f49cbc582f19
Abhijeet2010 <abhijeetraut220@gmail.com>
2024-01-02 13:09:54 +0530

    modified css SingleRestraurant, ItemList

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const ItemList = ({ data }) => {
+  return (
+    <>
+      {data?.map((a) => {
+        const IMG_URL =
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+        return (
+          <div key={a.card.info.id}>
+            <div className="flex gap-8 ">
+              <div className=" border-b-2 w-10/12  p-3 ">
+                <span
+                  className={
+                    a.card.info.isVeg
+                      ? "border-2 border-green-500 w-4 h-4 mb-2 flex justify-center items-center"
+                      : "border-2 border-red-500 w-4 h-4 mb-2   flex justify-center items-center"
+                  }
+                >
+                  <span
+                    className={
+                      a.card.info.isVeg
+                        ? "bg-green-400 w-2 h-2 inline-block rounded-full"
+                        : " bg-red-500 w-2 h-2 inline-block  rounded-full"
+                    }
+                  ></span>
+                </span>
+
+                <h3 className="font-semibold">{a.card.info.name}</h3>
+                <span>
+                  Rs.{a.card.info.price / 100 || a.card.info.defaultPrice / 100}
+                </span>
+                <p className="mt-4 font-light text-sm text-wrap text-justify">
+                  {a.card.info.description}
+                </p>
+              </div>
+
+              <div className="relative w-2/12 my-3 flex flex-col items-center justify-center">
+                <img
+                  className=" rounded-lg  object-cover"
+                  src={IMG_URL + a.card.info.imageId}
+                  alt="Menu-Items"
+                />
+                <span className="absolute bottom-5 bg-white px-4 rounded-md">
+                  Add+
+                </span>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default ItemList;
--- src/pages/SingleRestraurant.jsx
+++ src/pages/SingleRestraurant.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ItemList from "../components/ItemList";
+import Shimmer from "../components/Shimmer";
 
 const SingleRestaurant = () => {
   const [showData, setShowData] = useState(false);
   const [menuData, setMenuData] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
 
   const getResDetails = async () => {
+    setLoading(true);
     const API =
       "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.667347&lng=73.8071448&restaurantId=" +
       id;
@@ -19,6 +23,7 @@ const SingleRestaurant = () => {
         (c) => c?.card?.card["@type"].includes("ItemCategory")
       );
     setMenuData(filterCategories);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -31,41 +36,24 @@ const SingleRestaurant = () => {
 
   return (
     <>
-      {menuData?.map((item, index) => {
-        const IMG_URL =
-          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+      {loading ? (
+        <Shimmer />
+      ) : (
+        menuData?.map((item, index) => {
+          return (
+            <div className="w-7/12 mx-auto p-3 border-b-2 " key={index}>
+              <h2
+                className="font-semibold text-2xl p-2 cursor-pointer"
+                onClick={handleClick}
+              >
+                {item.card.card.title}
+              </h2>
 
-        return (
-          <div className="w-6/12 mx-auto p-3 border-b-2  my-10" key={index}>
-            <h2
-              className="font-semibold text-2xl p-2 cursor-pointer"
-              onClick={handleClick}
-            >
-              {item.card.card.title}
-            </h2>
-
-            {showData &&
-              item?.card?.card?.itemCards?.map((a) => (
-                <div className="w-full flex border-2 my-2" key={a.card.info.id}>
-                  <div className="w-10/12 shadow-md p-2">
-                    <h3 className="text-lg font-bold">{a?.card?.info?.name}</h3>
-                    <p>{a.card.info.description}</p>
-                    <span className="font-semibold">
-                      ₹ {a.card.info.price / 100}.00
-                    </span>
-                  </div>
-                  <div className="w-2/12">
-                    <img
-                      className="h-28 w-full"
-                      src={IMG_URL + a.card.info.imageId}
-                      alt="resMenu"
-                    />
-                  </div>
-                </div>
-              ))}
-          </div>
-        );
-      })}
+              {showData && <ItemList data={item?.card?.card?.itemCards} />}
+            </div>
+          );
+        })
+      )}
     </>
   );
 };
